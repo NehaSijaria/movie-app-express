5b6878945a660393b3c5ae35afc1e9955ca24c46
Neha <nehasijaria@gmail.com>
2022-03-20 01:43:37 -0400

    create get/post

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
+app.use(express.json());
+
+const genres= [
+  { id:1, name:"comedy"},
+   { id:1, name:"classic"}
+]
 
 app.get('/',(req,res)=>{
-  res.send('hello')
+  res.send(genres)
 })
+
+app.post("/api/genres", (req, res) => { 
+  const sendData = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  //create & push to existing
+  genres.push(sendData);
+  console.log(sendData); //{ id: 3, name: 'oldies' }
+  res.send(sendData);
+});
+
+
+app.put("/api/genres/:id", (req, res) => {
+
+});
+
 
 app.listen(port, ()=>{
   console.log(`App listening on ${port} `);
